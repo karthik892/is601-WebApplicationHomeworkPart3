2c0ec9fb791ea9b3af0fa0a8204d5f1da4729577
Karthik Sankaran <karthik892@users.noreply.github.com>
2021-04-16 11:45:48 -0400

    Switched from mysql-connector to flask-mysql

--- app/app.py
+++ app/app.py
@@ -1,44 +1,25 @@
 from typing import List, Dict
-import mysql.connector
 import simplejson as json
 from flask import Flask, Response, render_template
+from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
 
 app = Flask(__name__)
+mysql = MySQL(cursorclass=DictCursor)
 
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'citiesData'
+mysql.init_app(app)
 
-def cities_import() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'db',
-        'port': '3306',
-        'database': 'citiesData'
-    }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor(dictionary=True)
-
-    cursor.execute('SELECT * FROM tblCitiesImport')
-    result = cursor.fetchall()
-
-    cursor.close()
-    connection.close()
-
-    return result
-
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    user = {'username': 'Miguel'}
-    cities_data = cities_import()
-
-    return render_template('index.html', title='Home', user=user, cities=cities_data)
-
-
-@app.route('/api/cities')
-def cities() -> str:
-    js = json.dumps(cities_import())
-    resp = Response
-
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM mlb_players')
+    result = cursor.fetchall()
+    return render_template('index.html', title='Home', mlb=result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
