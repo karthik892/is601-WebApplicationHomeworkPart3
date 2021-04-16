e077650ffc43b9883be8b607990c418ecc86fb49
Karthik Sankaran <karthik892@users.noreply.github.com>
2021-04-16 13:37:45 -0400

    Added logic to save edited records

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from typing import List, Dict
 import simplejson as json
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, redirect, request
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
 
@@ -36,7 +36,17 @@ def form_edit_get(mlb_id):
     result = cursor.fetchall()
     return render_template('edit.html', title='Edit Form', mlb=result[0])
 
-
+@app.route('/edit/<int:mlb_id>', methods=['POST'])
+def form_update_post(mlb_id):
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('fldName'), request.form.get('fidTeam'), request.form.get('fidPosition'),
+                 request.form.get('fidWeight'), request.form.get('fidHeight'),
+                 request.form.get('fidAge'), mlb_id)
+    sql_update_query = """UPDATE mlb_players t SET t.Name = %s, t.Team = %s, t.Position = %s, t.Weight_lbs = 
+    %s, t.Height_inches = %s, t.Age = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
